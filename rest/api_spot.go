21efe3bb0b9923dc20f3fb4e6548807621156c42
Kianrad <kianrad@gmail.com>
2022-08-27 00:03:35 +0200

    fix create order params

--- rest/api_spot.go
+++ rest/api_spot.go
@@ -19,9 +19,21 @@ func (b *ByBit) CreateSpotOrder(symbol string,
 	side string,
 	orderType string,
 	qty float64,
-	timeInForce string) (query string, resp []byte, result Balance, err error) {
+	price float64,
+	timeInForce string,
+	clientOID string,
+) (query string, resp []byte, result Balance, err error) {
 	var ret GetBalanceResult
 	params := map[string]interface{}{}
+	params["symbol"] = symbol
+	params["side"] = side
+	params["type"] = orderType
+	if price > 0 {
+		params["price"] = price
+	}
+	params["timeInForce"] = timeInForce
+	params["qty"] = qty
+	params["orderLinkId"] = clientOID
 	query, resp, err = b.SignedRequest(http.MethodGet, "/spot/v1/order", params, &ret)
 	if err != nil {
 		return
