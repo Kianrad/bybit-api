2d91ebbfcb8f0f95e8e077de5cde23b22f34d5bc
Kianrad <kianrad@gmail.com>
2022-08-26 23:41:01 +0200

    add create order

--- rest/api_spot.go
+++ rest/api_spot.go
@@ -14,3 +14,17 @@ func (b *ByBit) GetAccount() (query string, resp []byte, result Balance, err err
 	}
 	return
 }
+
+func (b *ByBit) CreateSpotOrder(symbol string,
+	side string,
+	orderType string,
+	qty float64,
+	timeInForce string) (query string, resp []byte, result Balance, err error) {
+	var ret GetBalanceResult
+	params := map[string]interface{}{}
+	query, resp, err = b.SignedRequest(http.MethodGet, "/spot/v1/order", params, &ret)
+	if err != nil {
+		return
+	}
+	return
+}
